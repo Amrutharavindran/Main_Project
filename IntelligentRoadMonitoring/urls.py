fd173b261e6e1936c0f2fb4c84583937b9c4eaea
Amrutharavindran <116252422+Amrutharavindran@users.noreply.github.com>
2024-05-07 09:13:32 +0530

    Add files via upload

---
+++ IntelligentRoadMonitoring/urls.py
@@ -0,0 +1,24 @@
+"""IntelligentRoadMonitoring URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("SAFE_DRIVE_APP.urls")),
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
